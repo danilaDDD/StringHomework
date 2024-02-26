7f449aa928990ae324ec2e4fbf22dbc4f268fc9a
danila nikitin <danila.n2015@yandex.ru>
2024-02-26 18:07:18 +0300

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Задайте произвольную строку. 
+Выясните, является ли она палиндромом.
+*/
+
+void Main()
+{
+   System.Console.Write("Введите произвольную строку:");
+   string inputString = System.Console.ReadLine();
+   string reverseString = ReverseString(inputString);
+
+   if(inputString == reverseString)
+        System.Console.WriteLine("Да");
+    else
+        System.Console.WriteLine("Нет");
+    
+}
+
+string ReverseString(string inputString)
+{   
+    int strLength = inputString.Length;
+    char[] charArray = new char[strLength];
+
+    for(int i = 0; i < strLength; i++)
+    {
+        charArray[i] = inputString[strLength - 1 - i];
+    }
+
+    return string.Join("", charArray);
+}
+
+Main();
