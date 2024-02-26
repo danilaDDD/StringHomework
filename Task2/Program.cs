6f76e2f68ee59edc494628a733f0dfef9b0b487b
danila nikitin <danila.n2015@yandex.ru>
2024-02-26 17:46:14 +0300

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+Задайте строку, содержащую латинские буквы
+в обоих регистрах. Сформируйте строку, в которой все
+заглавные буквы заменены на строчные. 
+*/
+
+void Main()
+{
+    System.Console.Write("Введите строку из латинских символов:");
+    string inputString = System.Console.ReadLine();
+    System.Console.WriteLine(inputString.ToLower());
+}
+
+Main();
